19670243d27d42a3ee577b7d3a5e7deaddafbb0f
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2025-05-16 22:59:58 -0300

    feature/#2/leitura em andamento

--- app/Http/Controllers/PreparacaoDados/PreparacaoDadosController.php
+++ app/Http/Controllers/PreparacaoDados/PreparacaoDadosController.php
@@ -10,6 +10,10 @@ class PreparacaoDadosController extends Controller
 {
     public function preparar(MainRequest $request)
     {
-
+        //$regex = '^[ABCDEFG][^\s]*\s';//tudo até o primeiro espaço.
+        $regex = '[ABCDEFG](.)*[\t\n\s\r]';//tudo até o primeiro espaço.
+        $t = $request->get('texto').' ';
+        preg_match_all('/'.$regex.'/ ', $t, $matches, PREG_OFFSET_CAPTURE);
+        dd($matches);
     }
 }
